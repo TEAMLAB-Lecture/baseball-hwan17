e74f31a479cc5861782b3c761d1d3a5ce493174c
hwan17 <kimmh6017@gmail.com>
2021-01-19 23:40:36 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -31,8 +31,11 @@ def is_digit(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-    result = None
-
+    try:
+        int(user_input_number)
+        result = True
+    except:
+        result = False
     # ==================================
     return result
 
@@ -58,8 +61,7 @@ def is_between_100_and_999(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-    result = None
-
+    result = int(user_input_number)>=100 and int(user_input_number)<1000
     # ==================================
     return result
 
@@ -86,8 +88,7 @@ def is_duplicated_number(three_digit):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    result = len(set(str(three_digit))) < 3
     # ==================================
     return result
 
@@ -114,8 +115,7 @@ def is_validated_number(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    result = is_digit(user_input_number) and is_between_100_and_999(user_input_number) and not is_duplicated_number(user_input_number)
     # ==================================
     return result
 
@@ -141,8 +141,9 @@ def get_not_duplicated_three_digit_number():
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
     # get_random_number() 함수를 사용하여 random number 생성
-
-    result = None
+    result = get_random_number()
+    while is_duplicated_number(result):
+        result = get_random_number()
     # ==================================
     return result
 
@@ -174,8 +175,13 @@ def get_strikes_or_ball(user_input_number, random_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    strikes, ball = 0,0
+    for idx in range(3):
+        if user_input_number[idx] == random_number[idx]:
+            strikes += 1
+        elif user_input_number[idx] in random_number:
+            ball += 1
+    result = [strikes,ball]
     # ==================================
     return result
 
@@ -206,8 +212,8 @@ def is_yes(one_more_input):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    one_more_input_low = one_more_input.lower()
+    result = one_more_input_low == 'y' or one_more_input_low == 'yes'
     # ==================================
     return result
 
@@ -238,17 +244,37 @@ def is_no(one_more_input):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-
-    result = None
+    one_more_input_low = one_more_input.lower()
+    result = one_more_input_low == 'n' or one_more_input_low == 'no'
     # ==================================
     return result
 
 
 def main():
     print("Play Baseball")
-    user_input = 999
+    # user_input = 999
     random_number = str(get_not_duplicated_three_digit_number())
     print("Random Number is : ", random_number)
+    play_game = True
+    while play_game:
+        user_input = input('Input guess number : ')
+        if user_input == '0':
+            break
+        if not is_validated_number(user_input):
+            print('Wrong Input, Input again')
+            continue
+        strikes, balls = get_strikes_or_ball(user_input, random_number)
+        print(f'Strikes : {strikes} , Balls : {balls}')
+        if strikes == 3:
+            y_n = input('You win, one more(Y/N) ?')
+            while not is_no(y_n) and not is_yes(y_n):
+                print('Wrong Input, Input again')
+                y_n = input('You win, one more(Y/N) ?')
+            if is_no(y_n):
+                play_game = False
+            else:
+                random_number = str(get_not_duplicated_three_digit_number())
+                print("Random Number is : ", random_number)
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
 
